d439b0419de4ec3b7a15fd20003826767f89a85d
lojaecompack-hash <lojaecompack@gmail.com>
2026-01-16 17:25:58 -0300

    revert: Reverter mudança no schema - Aplicar fix diretamente no banco via SQL

--- remove-unique-operator-email.sql
+++
@@ -1,4 +0,0 @@
--- Remover constraint UNIQUE do campo email da tabela Operator
--- Isso permite criar múltiplos operadores sem email (NULL)
-
-ALTER TABLE "Operator" DROP CONSTRAINT IF EXISTS "Operator_email_key";
